8977b2f4c5251f066f38127677d4a93966614e69
Luis Miguel  García Velázquez <luis@iesmhp.local>
2024-03-11 17:23:00 +0100

    bootstrap

--- src/index.php
+++ src/index.php
@@ -19,14 +19,16 @@ $result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC");
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">	
 	<title>Panel de control</title>
-<!--	
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
--->	
+	<!--bootstrap-->
+	<!--
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+	-->	
 </head>
 <body>
-<!--	
+<!--Bootstrap-->	
+<!--
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
--->	
+-->
 <div>
 	<header>
 		<h1>Panel de Control</h1>
